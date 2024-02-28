4517188202e7948e63110e1bbf5b1261099d8a2c
Jorge Flores <flores.jorgeluis@gmail.com>
2024-02-27 20:58:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-
+# Load configurations from settings.yaml
 gauth = GoogleAuth()
-#Path completo donde se encuentra el archivo client_secrets.json
-gauth.settings['client_config_file'] = ''
+gauth.settings['client_config_file'] = 'D:/Playground/Python/itesm/client_secrets.json'
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
 #Folder ID de alguna carpeta en nuestra cuenta de google drive donde subiremos el archivo.
-folder = ''
+folder = '1Qd6Tqh4EKJngfIQO4OGmxO2WyEbruukK'
 
-# ejemplo de como subir un archivo nuevo 
+
 #file1 = drive.CreateFile({'parents': [{'id': folder}], 'title': 'hello.txt'})
 #file1.SetContentString("Hello World")
 #file1.Upload()
 
-# Directorio donde leeremos el archivo que vamos a subir.
-directory = ""
+directory = "D:/Playground/Python/itesm/datos/"
 for f in os.listdir(directory):
     filename = os.path.join(directory, f)
-    gfile = drive.CreateFile({'parents': [{'id': folder}], 'title': f})
-    gfile.SetContentFile(filename)
-    gfile.Upload()
+    # Revisa si el archivo ya existe en Google Drive
+    file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(folder)}).GetList()
+    for file in file_list:
+        if file['title'] == f:
+            # If the file exists, update its content
+            file.SetContentFile(filename)
+            file.Upload()
+            break
+    else:
+        # Si el archivo no existe crea uno nuevo.
+        gfile = drive.CreateFile({'parents': [{'id': folder}], 'title': f})
+        gfile.SetContentFile(filename)
+        gfile.Upload()
